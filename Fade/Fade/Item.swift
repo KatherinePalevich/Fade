2ae504519c083ce4b9d28b8947f1205a097f7ed2
KatherinePalevich <katherine.palevich@gmail.com>
2026-04-15 14:14:11 -0700

    Initial commit of app code

---
+++ Fade/Fade/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Fade
+//
+//  Created by Katherine Palevich on 4/15/26.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
